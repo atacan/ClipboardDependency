6efa9d8566a7c45057dd69d351a83231a6fb5d11
atacan <atacandur@icloud.com>
2023-11-18 12:27:23 +0100

    initial

--- Package.swift
+++ Package.swift
@@ -1,21 +1,30 @@
-// swift-tools-version: 5.9
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "ClipboardDependency",
+    platforms: [
+        .macOS(.v13),
+        .iOS(.v15)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "ClipboardDependency",
             targets: ["ClipboardDependency"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-dependencies", from: "1.0.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ClipboardDependency"),
+            name: "ClipboardDependency", dependencies: [
+                .product(name: "Dependencies", package: "swift-dependencies"),
+            ]),
         .testTarget(
             name: "ClipboardDependencyTests",
             dependencies: ["ClipboardDependency"]),
--- Sources/ClipboardDependency/ClipboardDependency.swift
+++ Sources/ClipboardDependency/ClipboardDependency.swift
@@ -1,2 +1,44 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+import Dependencies
+
+public struct ClipboardClient {
+    public var copyString: (String) -> Void
+    public var getString: () -> String?
+}
+
+#if os(macOS)
+    import Cocoa
+
+    extension ClipboardClient: DependencyKey {
+        public static var liveValue: Self {
+            Self(
+                copyString: { text in
+                    NSPasteboard.general.clearContents()
+                    NSPasteboard.general.setString(text, forType: .string)
+                },
+                getString: { NSPasteboard.general.string(forType: .string) }
+            )
+        }
+    }
+#endif
+
+#if os(iOS)
+    import UIKit
+
+    extension ClipboardClient: DependencyKey {
+        public static var liveValue: Self {
+            Self(
+                copyString: { text in
+                    UIPasteboard.general.string = text
+                },
+                getString: { UIPasteboard.general.string }
+            )
+        }
+    }
+#endif
+
+extension DependencyValues {
+    public var clipboard: ClipboardClient.Value {
+        get { self[ClipboardClient.self] }
+        set { self[ClipboardClient.self] = newValue }
+    }
+}
