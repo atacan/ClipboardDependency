18909286d0088ffdfb1e6a6bd3b6a98801cbfd37
atacan <atacandur@icloud.com>
2024-10-03 14:13:48 +0200

    get copy NSAttributedString

--- Sources/ClipboardDependency/ClipboardDependency.swift
+++ Sources/ClipboardDependency/ClipboardDependency.swift
@@ -1,8 +1,11 @@
 import Dependencies
+import Foundation
 
 public struct ClipboardClient {
     public var copyString: (String) -> Void
     public var getString: () -> String?
+    public var copyAttributedString: (NSAttributedString) -> Void
+    public var getAttributedString: () -> NSAttributedString?
 }
 
 #if os(macOS)
@@ -15,7 +18,14 @@ public struct ClipboardClient {
                     NSPasteboard.general.clearContents()
                     NSPasteboard.general.setString(text, forType: .string)
                 },
-                getString: { NSPasteboard.general.string(forType: .string) }
+                getString: { NSPasteboard.general.string(forType: .string) },
+                copyAttributedString: { attributedText in
+                    NSPasteboard.general.clearContents()
+                    NSPasteboard.general.writeObjects([attributedText])
+                },
+                getAttributedString: {
+                    NSPasteboard.general.readObjects(forClasses: [NSAttributedString.self], options: nil)?.first as? NSAttributedString
+                }
             )
         }
     }
@@ -30,7 +40,17 @@ public struct ClipboardClient {
                 copyString: { text in
                     UIPasteboard.general.string = text
                 },
-                getString: { UIPasteboard.general.string }
+                getString: { UIPasteboard.general.string },
+                copyAttributedString: { attributedText in
+                    UIPasteboard.general.string = attributedText.string
+                    UIPasteboard.general.addItems([[NSAttributedString.Key.documentType: NSAttributedString.DocumentType.rtf,
+                                                    NSAttributedString.Key.rtf: try! attributedText.data(from: NSRange(location: 0, length: attributedText.length),
+                                                                                                         documentAttributes: [.documentType: NSAttributedString.DocumentType.rtf])]])
+                },
+                getAttributedString: {
+                    guard let data = UIPasteboard.general.data(forPasteboardType: "public.rtf") else { return nil }
+                    return try? NSAttributedString(data: data, options: [.documentType: NSAttributedString.DocumentType.rtf], documentAttributes: nil)
+                }
             )
         }
     }
